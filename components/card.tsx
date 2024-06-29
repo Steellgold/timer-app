57cb62a695d0ae678ffbc6d0a81f5b5ecbbedfa0
Gaëtan H <gaetanhus@gmail.com>
2024-06-29 22:11:16 +0200

    style(card.tsx): update import statements and add colorTheme prop to TimerCard component feat(card.tsx): add Popover component for color theme selection in TimerCard component feat(card.tsx): add toggleTheme function to update color theme in TimerCard component feat(card.tsx): add colorScheme function to handle dynamic class values based on colorTheme prop

--- components/card.tsx
+++ components/card.tsx
@@ -5,12 +5,16 @@ import { Component } from "./ui/component";
 import { DraggableProvided } from "@hello-pangea/dnd";
 import { CircularProgressbar, buildStyles } from "react-circular-progressbar";
 import { useEffect, useRef, useState } from "react";
-import { Bell, Pause, Pin, PinOff, Play, Scan, X } from "lucide-react";
+import { Bell, PaintBucket, Pause, Pin, PinOff, Play, Scan, X } from "lucide-react";
 import { cn } from "@/lib/utils";
 import { dayJS } from "@/lib/dayjs/day-js";
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+import { ClassValue } from "clsx";
 
-export const TimerCard: Component<Timer & { provided: DraggableProvided }> = ({
+export const TimerCard: Component<Timer & {
+  provided: DraggableProvided
+}> = ({
   provided,
   pinned,
   startAt,
@@ -18,9 +22,10 @@ export const TimerCard: Component<Timer & { provided: DraggableProvided }> = ({
   isPaused,
   isEnded,
   title,
-  id
+  id,
+  colorTheme
 }) => {
-  const { timers, deleteTimer, togglePaused, toggleEnd, togglePinned, toggleFocused } = useTimers()
+  const { timers, deleteTimer, togglePaused, toggleEnd, togglePinned, toggleFocused, toggleTheme } = useTimers()
   const audio = useRef(new Audio('/sounds/soft.mp3'));
   
   const [timeLeft, setTimeLeft] = useState<number>((dayJS(endAt).diff(dayJS(), "seconds")));
@@ -66,60 +71,80 @@ export const TimerCard: Component<Timer & { provided: DraggableProvided }> = ({
       <Tooltip>
         <TooltipTrigger>
           <div ref={provided.innerRef} {...provided.draggableProps} {...(pinned ? {} : provided.dragHandleProps)}
-            className="bg-[#d1ccc7] dark:bg-[#131212] border border-[#201f1f1c] rounded-xl p-4 sm:w-[200px] h-[415px] sm:h-auto"
+            className={cn("bg-[#d1ccc7] dark:bg-[#131212] border border-[#201f1f1c] rounded-xl p-4 w-auto sm:w-auto sm:h-auto", {
+              "border-blue-400 dark:border-[#5363bb] bg-blue-50 dark:bg-[#5d72eb]": colorTheme === "blue",
+              "border-green-400 dark:border-[#3bbf4a] bg-green-50 dark:bg-[#3bbf4a]": colorTheme === "green",
+              "border-red-400 dark:border-[#bf3b3b] bg-red-50 dark:bg-[#bf3b3b]": colorTheme === "red",
+              "border-yellow-400 dark:border-[#bfbe3b] bg-yellow-50 dark:bg-[#bfbe3b]": colorTheme === "yellow",
+              "border-purple-400 dark:border-[#8e3bbf] bg-purple-50 dark:bg-[#8e3bbf]": colorTheme === "purple"
+            })}
           >
             <div className="flex justify-between items-center mb-2">
               {pinned ? (
-                <PinOff
-                  fill="currentColor"
-                  size={30}
-                  onClick={() => !isEnded && togglePinned(id)}
-                    className={cn(
-                    "cursor-pointer rounded-full p-1.5 text-primary-foreground",
-                    "dark:text-blue-800 bg-blue-50 dark:bg-blue-300",
-                    "hover:bg-blue-300 dark:hover:bg-blue-400", {
-                      "opacity-50 hover:cursor-not-allowed": isEnded
-                    }
-                  )}
-                />
+                <PinOff fill="currentColor" size={30} className={colorScheme(colorTheme, "cursor-pointer rounded-full p-1.5 text-primary-foreground", { "opacity-50 hover:cursor-not-allowed": isEnded })} />
               ) : (timers.some(timer => timer.pinned) ? (
-                <PinOff
-                  fill="currentColor"
-                  size={30}
-                  className={cn(
-                    "opacity-10",
-                    "cursor-pointer rounded-full p-1.5 text-primary-foreground",
-                    "dark:text-gray-800 bg-gray-50 dark:bg-gray-300",
-                    "hover:bg-gray-300 dark:hover:bg-gray-400", {
-                      "hover:cursor-not-allowed": timers.some(timer => timer.pinned)
-                    }
-                  )}
-                />
+                <PinOff fill="currentColor" size={30} className={colorScheme(colorTheme, "cursor-pointer rounded-full p-1.5 text-primary-foreground", { "opacity-50 hover:cursor-not-allowed": isEnded })} />
               ) :
-                <Pin
-                  fill="currentColor"
-                  size={30}
-                  onClick={() => !isEnded && togglePinned(id)}
-                  className={cn(
-                    "cursor-pointer rounded-full p-1.5 text-primary-foreground",
-                    "dark:text-blue-900 bg-blue-100 dark:bg-blue-400",
-                    "hover:bg-blue-200 dark:hover:bg-blue-300", {
-                      "opacity-50 hover:cursor-not-allowed": isEnded
-                    }
-                  )}
-                />
+                <Pin size={30} fill="currentColor" onClick={() => !isEnded && togglePinned(id)}
+                  className={colorScheme(colorTheme, "cursor-pointer rounded-full p-1.5 text-primary-foreground", { "opacity-50 hover:cursor-not-allowed": isEnded })} />
               )}
+
+              <Popover>
+                <PopoverTrigger asChild>
+                  <PaintBucket fill="currentColor" size={30}
+                    className={colorScheme(colorTheme, "cursor-pointer rounded-full p-1.5 text-primary-foreground", { "opacity-50 hover:cursor-not-allowed": isEnded })} />
+                </PopoverTrigger>
+
+                <PopoverContent className="p-4 space-y-2 ml-4">
+                  <div className="flex items-center space-x-2 w-full">
+                    <div className={cn("h-8 w-8 cursor-pointer rounded-full dark:bg-[#131212] bg-[#d1ccc7] border-[2px]", {
+                      "dark:border-[#201f1f1c] border-[#201f1f1c]": colorTheme === "default"
+                    })} onClick={() => toggleTheme(id, "default")} />
+
+                    <div className={cn("h-8 w-8 cursor-pointer rounded-full dark:bg-blue-400 bg-blue-100 border-[2px]", {
+                      "dark:border-blue-400 border-blue-100": colorTheme === "blue"
+                      })} onClick={() => toggleTheme(id, "blue")} />
+
+                    <div className={cn("h-8 w-8 cursor-pointer rounded-full dark:bg-green-400 bg-green-100 border-[2px]", {
+                      "dark:border-green-400 border-green-100": colorTheme === "green"
+                    })} onClick={() => toggleTheme(id, "green")} />
+
+                    <div className={cn("h-8 w-8 cursor-pointer rounded-full dark:bg-red-400 bg-red-100 border-[2px]", {
+                      "dark:border-red-400 border-red-100": colorTheme === "red"
+                    })} onClick={() => toggleTheme(id, "red")} />
+
+                    <div className={cn("h-8 w-8 cursor-pointer rounded-full dark:bg-yellow-400 bg-yellow-100 border-[2px]", {
+                      "dark:border-yellow-400 border-yellow-100": colorTheme === "yellow"
+                    })} onClick={() => toggleTheme(id, "yellow")} />
+
+                    <div className={cn("h-8 w-8 cursor-pointer rounded-full dark:bg-purple-400 bg-purple-100 border-[2px]", {
+                      "dark:border-purple-400 border-purple-100": colorTheme === "purple"
+                    })} onClick={() => toggleTheme(id, "purple")} />
+                  </div>
+                </PopoverContent>
+              </Popover>
             </div>
 
-            <div className="relative">
+            <div className="relative flex justify-center items-center">
               <CircularProgressbar
                 value={!isEnded ? pourcentage : 0}
                 styles={buildStyles({
-                  pathColor: '#fff',
-                  trailColor: '#201f1f',
+                  trailColor: colorTheme === "default" ?'#201f1f'
+                              : colorTheme === "blue" ? '#93c5fd'
+                              : colorTheme === "green" ? '#86efac'
+                              : colorTheme === "red" ? '#fca5a5'
+                              : colorTheme === "yellow" ? '#fde047'
+                              : '#d8b4fe',
+                     pathColor: colorTheme === "default" ? '#4b4a4a'
+                              : colorTheme === "blue" ? '#60a5fa'
+                              : colorTheme === "green" ? '#4ade80'
+                              : colorTheme === "red" ? '#f87171'
+                              : colorTheme === "yellow" ? '#facc15'
+                              : '#c084fc',
                   pathTransitionDuration: 0.5,
                   strokeLinecap: 'round',
                 })}
+                className="w-44 h-44 sm:w-40 sm:h-40"
                 strokeWidth={4}
               />
 
@@ -129,22 +154,19 @@ export const TimerCard: Component<Timer & { provided: DraggableProvided }> = ({
                   {dayJS(endAt).format("HH:mm")}
                 </div>
 
-                {!isEnded && <div className="text-1xl sm:text-2xl font-bold">
+                {!isEnded && <div className="text-2xl font-bold">
                   {formatRemainingTime(timeLeft)}
                 </div>}
-                {isEnded && <div className="text-1xl sm:text-2xl font-bold">00:00</div>}
+                {isEnded && <div className="text-2xl font-bold">00:00</div>}
               </div>
             </div>
 
             <div className="flex justify-between items-center">
               <X
                 fill="currentColor"
+                strokeWidth={4}
                 size={30}
-                className={cn(
-                  "cursor-pointer rounded-full p-1.5 text-primary-foreground",
-                  "dark:text-red-900 bg-red-100 dark:bg-red-400",
-                  "hover:bg-red-200 dark:hover:bg-red-300"
-                )}
+                className={colorScheme(colorTheme, "cursor-pointer rounded-full p-1.5 text-primary-foreground")}
                 onClick={() => {
                   deleteTimer(id);
                   audio.current.pause();
@@ -157,26 +179,14 @@ export const TimerCard: Component<Timer & { provided: DraggableProvided }> = ({
                   fill="currentColor"
                   size={30}
                   onClick={() => !isEnded && togglePaused(id)}
-                  className={cn(
-                    "cursor-pointer rounded-full p-1.5 text-primary-foreground",
-                    "dark:text-yellow-900 bg-yellow-100 dark:bg-yellow-400",
-                    "hover:bg-yellow-200 dark:hover:bg-yellow-300", {
-                      "opacity-50 hover:cursor-not-allowed": isEnded
-                    }
-                  )}
+                  className={colorScheme(colorTheme, "cursor-pointer rounded-full p-1.5 text-primary-foreground", { "opacity-50 hover:cursor-not-allowed": isEnded})}
                 />
               ) : (
                 <Play
                   fill="currentColor"
                   size={30}
                   onClick={() => !isEnded && togglePaused(id)}
-                  className={cn(
-                    "cursor-pointer rounded-full p-1.5 text-primary-foreground",
-                    "dark:text-green-900 bg-green-100 dark:bg-green-400",
-                    "hover:bg-green-200 dark:hover:bg-green-300", {
-                    "opacity-50 hover:cursor-not-allowed": isEnded
-                    }
-                  )}
+                  className={colorScheme(colorTheme, "cursor-pointer rounded-full p-1.5 text-primary-foreground", { "opacity-50 hover:cursor-not-allowed": isEnded})}
                 />
               )}
             </div>
@@ -188,4 +198,15 @@ export const TimerCard: Component<Timer & { provided: DraggableProvided }> = ({
       </Tooltip>
     </TooltipProvider>
   )
+}
+
+const colorScheme = (colorTheme: Timer["colorTheme"], ...inputs: ClassValue[]): string => {
+  return cn(...inputs, {
+    "text-gray-900 dark:text-gray-900 hover:bg-gray-200 dark:hover:bg-gray-300 bg-gray-100 dark:bg-gray-400": colorTheme === "default",
+    "text-blue-900 dark:text-blue-900 hover:bg-blue-200 dark:hover:bg-blue-300 bg-blue-100 dark:bg-blue-400": colorTheme === "blue",
+    "text-yellow-900 dark:text-yellow-900 hover:bg-yellow-200 dark:hover:bg-yellow-300 bg-yellow-100 dark:bg-yellow-400": colorTheme === "yellow",
+    "text-green-900 dark:text-green-900 hover:bg-green-200 dark:hover:bg-green-300 bg-green-100 dark:bg-green-400": colorTheme === "green",
+    "text-red-900 dark:text-red-900 hover:bg-red-200 dark:hover:bg-red-300 bg-red-100 dark:bg-red-400": colorTheme === "red",
+    "text-purple-900 dark:text-purple-900 hover:bg-purple-200 dark:hover:bg-purple-300 bg-purple-100 dark:bg-purple-400": colorTheme === "purple"
+  });
 }
